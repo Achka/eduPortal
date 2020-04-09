4dcce07b34684895ab8af88d12ff2de5df3af935
Bohdan Shchur <bogdanshur1@gmail.com>
2020-04-09 11:54:17 +0300

    Add application db context

---
+++ EduPortalBackend/EduPortalBackend.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EduPortalBackend.DataAccess
+{
+	public class ApplicationDbContext : DbContext {
+		public DbSet<User> Users { get; set; }
+		// Contacts which should be added
+		//public DbSet<Contact> Contacts { get; set; }
+		public DbSet<Course> Courses { get; set; }
+		public DbSet<Material> Materials { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+			// environment dependant connection string must be here instead of hardcoded local db one
+			optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EduPortalDb;Trusted_Connection=True;");
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder) {
+			// Contacts which should be added(many-to-many)
+			//modelBuilder.Entity<Contact>().HasKey(contact => new { contact.UserB, contact.UserBId });
+			//modelBuilder.Entity<Contact>().HasOne(contact => contact.UserA)
+			//	.WithMany(user => user.Contacts).HasForeignKey(contact => contact.UserAId);
+			//modelBuilder.Entity<Contact>().HasOne(contact => contact.UserB)
+			//	.WithMany(user => user.Contacts).HasForeignKey(contact => contact.UserBId);
+		}
+	}
+}
