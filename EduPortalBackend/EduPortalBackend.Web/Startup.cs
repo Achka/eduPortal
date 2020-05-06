744463966f8093f4066acc9e9dff8d9d163f2a4c
Bohdan Shchur <bogdanshur1@gmail.com>
2020-05-06 16:46:39 +0300

    Make web project more loosy coupled
    Remove obsolete references and reference only Services and Contracts project

--- EduPortalBackend/EduPortalBackend.Web/Startup.cs
+++ EduPortalBackend/EduPortalBackend.Web/Startup.cs
@@ -1,10 +1,9 @@
-using EduPortalBackend.DataAccess;
-using Entities.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Services;
 
 namespace EduPortalBackend.Web
 {
@@ -18,8 +17,9 @@ namespace EduPortalBackend.Web
 
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services) {
-			services.AddIdentity<User, Role>().AddEntityFrameworkStores<ApplicationDbContext>();
-			services.AddDbContext<ApplicationDbContext>();
+			services.ConfigureDbContext();
+			services.ConfigureIdentity();
+			services.ConfigureUnitOfWork();
 			services.AddControllers();
 		}
 
---
+++ EduPortalBackend/Services/ServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Contracts;
+using DataAccess;
+using EduPortalBackend.DataAccess;
+using Entities.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Services
+{
+	public static class ServiceExtensions
+	{
+		public static void ConfigureUnitOfWork(this IServiceCollection services) => services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+		public static void ConfigureIdentity(this IServiceCollection services) => 
+			services.AddIdentity<User, Role>().AddEntityFrameworkStores<ApplicationDbContext>();
+
+		public static void ConfigureDbContext(this IServiceCollection services) => services.AddDbContext<ApplicationDbContext>();
+	}
+}
