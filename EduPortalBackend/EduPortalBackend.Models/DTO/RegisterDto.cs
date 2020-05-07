748309418b73177479c1e782439266e7dcbf7e78
Bohdan Shchur <bogdanshur1@gmail.com>
2020-05-07 12:41:45 +0300

    Add login and register endpoints

---
+++ EduPortalBackend/EduPortalBackend.Models/DTO/RegisterDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entities.DTO
+{
+	public class RegisterDto
+	{
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
+
+		[Required]
+		public string FirstName { get; set; }
+
+		[Required]
+		public string LastName { get; set; }
+
+		[DataType(DataType.Date)]
+		public DateTime? StudyingStart { get; set; }
+
+		[DataType(DataType.Date)]
+		public DateTime? StudyingFinish { get; set; }
+
+		[Required]
+		[StringLength(maximumLength: 100, MinimumLength = 6)]
+		public string Password { get; set; }
+	}
+}
---
+++ EduPortalBackend/EduPortalBackend.Web/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading.Tasks;
+using Entities.DTO;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace Web.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]/[action]")]
+	public class AccountController : Controller
+	{
+		private readonly SignInManager<User> signInManager;
+		private readonly UserManager<User> userManager;
+		private readonly AuthService authService;
+
+		public AccountController(SignInManager<User> signInManager, UserManager<User> userManager, AuthService authService) => 
+			(this.signInManager, this.userManager, this.authService) = (signInManager, userManager, authService);
+
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginDto model) {
+			var result = await this.signInManager.PasswordSignInAsync(model.Email, model.Password,
+				isPersistent: model.RememberMe, lockoutOnFailure: false);
+			if (!result.Succeeded) {
+				return BadRequest(new { errorText = "Invalid email or password" });
+			}
+
+			var user = await this.userManager.FindByEmailAsync(model.Email);
+			var token = this.authService.GenerateJwtToken(user);
+			return Json(new {
+				access_token = token
+			});
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Register(RegisterDto model) {
+			var user = new User {
+				FirstName = model.FirstName,
+				LastName = model.LastName,
+				UserName = model.Email,
+				Email = model.Email,
+				StudyingStart = model.StudyingStart,
+				StudyingFinish = model.StudyingFinish
+			};
+			var result = await this.userManager.CreateAsync(user, model.Password);
+			if (!result.Succeeded) {
+				return BadRequest(result.Errors);
+			}
+
+			await this.signInManager.SignInAsync(user, false);
+			var token = this.authService.GenerateJwtToken(user);
+			return Json(new {
+				access_token = token
+			});
+		}
+	}
+}
