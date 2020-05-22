5206ca702f7096bca464655d6f7b20885867aae0
Bohdan Shchur <bogdanshur1@gmail.com>
2020-05-22 18:07:14 +0300

    Add PUT endpoint for updating profile, use Automapper for endpoints

--- EduPortalBackend/EduPortalBackend.Web/Controllers/ProfileController.cs
+++ EduPortalBackend/EduPortalBackend.Web/Controllers/ProfileController.cs
@@ -1,9 +1,12 @@
-﻿using Entities.Models;
-using Entities.TransformationExtensions;
+﻿using AutoMapper;
+using Entities.DTO;
+using Entities.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Services.StandardResponses;
+using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Web.Controllers
@@ -14,8 +17,12 @@ namespace Web.Controllers
 	public class ProfileController : Controller
 	{
 		private readonly UserManager<User> userManager;
+		private readonly IMapper mapper;
 
-		public ProfileController(UserManager<User> userManager) => this.userManager = userManager;
+		public ProfileController(UserManager<User> userManager, IMapper mapper) {
+			this.userManager = userManager;
+			this.mapper = mapper;
+		}
 
 		[HttpGet("{id}")]
 		public async Task<IActionResult> GetProfile(long id) {
@@ -24,7 +31,29 @@ namespace Web.Controllers
 				return NotFound(new ApiResponse(404, $"User with id {id} not found"));
 			}
 
-			return Ok(new ApiOkResponse(await user.Transform(this.userManager)));
+			var userDto = this.mapper.Map<UserProfileDto>(user);
+			userDto.Role = (await this.userManager.GetRolesAsync(user)).Single();
+			return Ok(new ApiOkResponse(userDto));
+		}
+
+		[HttpPut("{id}")]
+		public async Task<IActionResult> UpdateProfile(long id, [FromBody]UserProfileDtoForUpdate userDto) {
+			if (id != long.Parse(this.User.FindFirstValue(ClaimTypes.NameIdentifier))) {
+				return StatusCode(403, new ApiResponse(403, "Write access to other user account is forbidden"));
+			}
+
+			var user = await this.userManager.FindByIdAsync(id.ToString());
+			if (user == null) {
+				return NotFound(new ApiResponse(404, $"User with id {id} not found"));
+			}
+
+			user = this.mapper.Map(userDto, user);
+			var result = await this.userManager.UpdateAsync(user);
+			if (!result.Succeeded) {
+				return StatusCode(500, new ApiResponse(500));
+			}
+
+			return NoContent();
 		}
 	}
 }
